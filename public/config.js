8643c63d1d8dcd72cb4dc7662f6b5dfd643c7c28
Anschluss-zeit <111547592+Anschluss-zeit@users.noreply.github.com>
2023-05-13 21:29:04 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,31 +6,41 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    //主服务器
+    'm794351942-a0a87d291bdbc15da7bf854c',
+    //主服务器 - SSH
+    'm794351942-a0a87d291bdbc15da7bf854c',
+    //主服务器 - Database
+    'm794351958-734bbdb0965b8935e61e5757',
+    //中转服务器 - Caddy
+    'm794351984-da4828b8869f2d311bff0026',
+    //中转服务器 - Server
+    'm794351992-317019e5f188615657fecf29',
+    //中转服务器 - Caddy - Web
+    'm794351996-1e4710cc2bf99fa474b981df',
+    //皮肤站
+    'm794351944-6eceab1bbe8bfa95c4293681',
+    //Yggdrasil API
+    'm794351859-65698878368bc2087b3746ee',
+    //网页地图 - Survival-2
+    'm794351903-d901ab3ff3ba23b162416378',
+    //MCSM - Web
+    'm794351951-d19ded5956b992be592292fd',
+    //MCSM - Daemon
+    'm794351953-f5996136469103456c3968ca',
   ],
 
   // 日志天数
   CountDays: 90,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
+      text: '皮肤站',
+      url: 'https://skin.lolw.top/'
+    }
   ],
 };
